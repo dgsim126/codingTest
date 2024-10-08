3074d1461598cda5b227cc9a0c2e9071d66e46e6
shipleaf <162288294+shipleaf@users.noreply.github.com>
2024-10-08 15:48:56 +0900

    241008

--- lv2/241008/카펫.py
+++ lv2/241008/카펫.py
@@ -1,15 +1,15 @@
 def solution(brown, yellow):
-    wh_list = []
+    wh_list = []    # brown과 yellow로 만들 수 있는 w * h 조합
     answer = []
     for i in range(3, int((brown+yellow)**0.5)+1):
         if (brown + yellow) % i == 0:
             w = i
             h = (brown+yellow)//i
             if w < h:
-                w, h = h, w
+                w, h = h, w     # wh의 0번 index가 무조건 w 값이 되도록
             wh = [w, h]
             wh_list.append(wh)
-    for i in wh_list:
+    for i in wh_list:   # yellow가 무조건 중앙에 오는지 확인
         if brown != ((i[0] * 2) + 2 * (i[1] - 2)) or yellow != ((i[0]-2) * (i[1]-2)):
             continue
         else:
